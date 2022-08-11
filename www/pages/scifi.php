61c6d77c2c1cb889544a7c2f5f0276a7acbc5352
sarah13310 <sarah13310@proton.me>
2022-08-11 21:56:30 +0200

    menu et pages

---
+++ www/pages/scifi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Freaks</title>
+    <link href="../css/main.css" rel="stylesheet">
+    <link href="../css/smoky.css" rel="stylesheet">
+</head>
+<body>
+<div class="title_site">
+    <span>F</span><span>R</span><span>E</span><span>A</span><span>K</span><span>S</span>
+</div>
+<div class="menu">
+    <ul class="item">
+        <li ><a href="" style="color:whitesmoke">Genre</a></li>
+        <li><a href="../pages/fantastic.php">Fantastique</a></li>
+        <li><a href="../pages/horror.php">Horreur</a></li>
+        <li><a href="../pages/scifi.php">Sci-fi</a></li>
+    </ul>
+</div>
+</body>
+</html>
