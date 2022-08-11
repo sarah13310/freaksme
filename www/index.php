a1f7f07952cfd8e34310ab24b9def6b304b3e002
sarah13310 <sarah13310@proton.me>
2022-08-11 21:07:25 +0200

    Initial master commit

---
+++ www/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Freaks</title>
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/smoky.css" rel="stylesheet">
+</head>
+<body>
+<div class="title_site">
+    <span>F</span><span>R</span><span>E</span><span>A</span><span>K</span><span>S</span>
+</div>
+<div class="menu">
+    <ul class="item">
+        <li ><a href="" style="color:whitesmoke">Genre</a></li>
+        <li><a href="#fantastic">Fantastique</a></li>
+        <li><a href="#horror">Horreur</a></li>
+        <li><a href="#scifi">Sci-fi</a></li>
+    </ul>
+</div>
+</body>
+</html>
