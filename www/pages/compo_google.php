36b096e54caba05d2c6b3309239681ae49b9078a
sarah13310 <sarah13310@proton.me>
2022-09-19 11:24:51 +0200

    version 1.7 Google maps

--- www/pages/compo_google.php
+++ www/pages/compo_google.php
@@ -1,4 +1,9 @@
-<div class="map-responsive">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92100.56867926556!2d4.272642858057628!3d43.832137558683186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b42d0bd6e85339%3A0xde88134f9f200c03!2zTsOubWVz!5e0!3m2!1sfr!2sfr!4v1484817447856"
-            width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+<div class="map-responsive-main">
+    <div class="map-responsive-caption">Nous rencontrer</div>
+    <div class="map-responsive-body">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.6455667764167!2d4.6207139148132965!3d43.67634065895323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b676404bd2fa4b%3A0xfd5ca95581952fe2!2s7%20Rue%20de%20la%20Roquette%2C%2013200%20Arles!5e0!3m2!1sfr!2sfr!4v1663525313652!5m2!1sfr!2sfr"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 </div>
